3cc22c419e5854498c0408eb29309b390ea229af
Will Bradshaw <wjbradshaw1@gmail.com>
2020-09-02 15:00:55 +0100

    Updated main-text strategy figure

--- figures_workflow/scripts/dev/main_strategies_dev.R
+++ figures_workflow/scripts/dev/main_strategies_dev.R
@@ -17,6 +17,27 @@ theme_base <- theme_base + theme(
   axis.title.y = element_text(margin = margin(r = 0.15, unit = "cm")),
   axis.title.x = element_text(margin = margin(t = 0.2, b = 0, unit = "cm"))
 )
+
+# Input paths
+#manual_path <- snakemake@input[["manual"]]
+#digital_path <- snakemake@input[["digital"]]
+#uptake_path <- snakemake@input[["uptake"]]
+#window_path <- snakemake@input[["window"]]
+manual_path <- "data/main_strategies_manual_1k_scenario.tsv.gz"
+digital_path <- "data/main_strategies_digital_1k_scenario.tsv.gz"
+uptake_path <- "data/main_strategies_uptake_1k_scenario.tsv.gz"
+window_path <- "data/si_window_median_1k_scenario.tsv.gz"
+
+
+# Output paths & parameters
+#plot_scale_cm <- snakemake@params[["panel_scale"]]
+#main_path <- snakemake@output[["main"]]
+#si_uptake_reff_path <- snakemake@output[["si_uptake_reff"]]
+plot_scale_cm <- 13
+plot_scale_in <- plot_scale_cm/2.54
+main_path <- "output_files/dev_main_strategies.png"
+si_uptake_reff_path <- "output_files/dev_si_uptake_reff.png"
+
 cat("done.\n")
 
 #==============================================================================
@@ -26,8 +47,6 @@ cat("done.\n")
 cat("\nReading in data...")
 
 # Manual tracing (with and without automated)
-#manual_path <- snakemake@input[["manual"]]
-manual_path <- "data/main_strategies_manual_1k_scenario.tsv.gz"
 manual_data <- suppressMessages(read_tsv(manual_path)) %>%
   mutate(backtrace_distance = factor(backtrace_distance, levels=c(0,Inf)),
          contact_limit_manual = factor(contact_limit_manual, levels=c(6,2)),
@@ -36,8 +55,6 @@ manual_data <- suppressMessages(read_tsv(manual_path)) %>%
          p_traced = p_traced_manual)
 
 # Digital tracing alone (universal or high coverage)
-#digital_path <- snakemake@input[["digital"]]
-digital_path <- "data/main_strategies_digital_1k_scenario.tsv.gz"
 digital_data <- suppressMessages(read_tsv(digital_path)) %>%
   mutate(backtrace_distance = factor(backtrace_distance, levels=c(0,Inf)),
          contact_limit_auto = factor(contact_limit_auto, levels=c(Inf,2)),
@@ -45,16 +62,13 @@ digital_data <- suppressMessages(read_tsv(digital_path)) %>%
          p_traced = p_traced_auto)
 
 # Uptake contour plots
-#uptake_path <- snakemake@input[["uptake"]]
-uptake_path <- "data/main_strategies_uptake_1k_scenario.tsv.gz"
 uptake_data <- suppressMessages(read_tsv(uptake_path)) %>% 
   filter(p_traced_manual == 0.9 | contact_limit_manual == 6) %>%
   prep_contour_data(0.9, "p_smartphone_overall", "p_data_sharing_auto",
                     c("p_traced_manual", "backtrace_distance", "contact_limit_manual"))
 
 # Window contour plots
-#window_path <- snakemake@input[["window"]]
-window_path <- "data/si_window_median_1k_scenario.tsv.gz"
+
 window_data <- suppressMessages(read_tsv(window_path)) %>% 
   filter(p_traced_auto != 0, p_smartphone_overall == 0.53,
          backtrace_distance == Inf) %>%
@@ -227,45 +241,6 @@ cat("\n...done.\n")
 #==============================================================================
 
 #------------------------------------------------------------------------------
-# Single-strategy control-rate plots
-#------------------------------------------------------------------------------
-
-cat("\nGenerating single-strategy control-rate plots...")
-
-cat("\n\tPreparing data...")
-si_ctrl_data_digital <- digital_data %>%
-  filter(p_smartphone_overall == 1.0, p_data_sharing_auto == 1.0) %>%
-  mutate(trace_type = "Digital tracing only\n(universal coverage)",
-         contact_limit = as.character(contact_limit_auto)) %>%
-  select(-contact_limit_auto)
-
-si_ctrl_data_manual <- manual_data %>%
-  filter(p_smartphone_overall == 0.8) %>%
-  mutate(trace_type = ifelse(trace_type == "Manual + digital tracing",
-                             "Manual + digital\n(hybrid) tracing",
-                             trace_type),
-         contact_limit = as.character(contact_limit_manual)) %>%
-  select(-contact_limit_manual)
-
-si_ctrl_data <- bind_rows(si_ctrl_data_digital, si_ctrl_data_manual) %>%
-  mutate(contact_limit = factor(contact_limit, levels=c("Inf", "6", "2")))
-
-cat("\n\tMaking plot...")
-si_ctrl <- ggplot(si_ctrl_data,
-                    aes(x=p_traced, y=p_controlled,
-                        ymin=p_controlled_lower, ymax=p_controlled_upper,
-                        colour=backtrace_distance, 
-                        linetype=contact_limit,
-                        shape=contact_limit)) %>%
-  format_ctrl(ylimits = c(0,0.5), coord_ratio = 2) %>% colour_backtrace %>% 
-  linetype_window %>% x_ptrace %>%
-  theme_external()
-
-si_ctrl <- si_ctrl + facet_grid(.~trace_type)
-
-cat("\n...done.\n")
-
-#------------------------------------------------------------------------------
 # Generalised contour plots
 #------------------------------------------------------------------------------
 
@@ -274,7 +249,7 @@ cat("\nGenerating supplementary contour plots...")
 # Format data
 cat("\n\tPreparing data...")
 si_uptake_data <- uptake_data %>%
-  mutate(row_lab = paste(bt_type, "tracing,\n"),
+  mutate(row_lab = paste(bt_type, "tracing"),
          col_lab = ifelse(trace_type == "Digital only", "Digital tracing",
                           paste0("Hybrid tracing\n(", trace_window_manual, ")")))
 
@@ -296,7 +271,7 @@ fwd_palette_sparse <- uptake_data %>%
 cat("\n\tMaking R_eff contour plots...")
 si_uptake_reff_bidir <- si_uptake_data %>% filter(bt_type == "Bidirectional") %>%
   contour_plot_reff_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = bidir_palette_dense, breaks = seq(0,10,step_dense)) +
+                        palette = bidir_palette_sparse, breaks = seq(0,10,step_sparse)) +
   scale_x_continuous(name = "% of cases with\nchirping smartphones",
                      labels = label_pc, breaks = seq(0,1,0.2)) +
   scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
@@ -305,7 +280,7 @@ si_uptake_reff_bidir <- si_uptake_data %>% filter(bt_type == "Bidirectional") %>
   theme_base + theme(legend.position="none")
 si_uptake_reff_fwd <- si_uptake_data %>% filter(bt_type == "Forward-only") %>%
   contour_plot_reff_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = fwd_palette_dense, breaks = seq(0,10,step_dense)) +
+                        palette = fwd_palette_sparse, breaks = seq(0,10,step_sparse)) +
   scale_x_continuous(name = "% of cases with\nchirping smartphones",
                      labels = label_pc, breaks = seq(0,1,0.2)) +
   scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
@@ -316,32 +291,6 @@ si_uptake_reff <- plot_grid(si_uptake_reff_fwd %>% strip_upper(),
                             si_uptake_reff_bidir %>% strip_lower(),
                             nrow = 2, align = "v", axis= "l")
 
-# Ctrl SI plot
-cat("\n\tMaking control-rate contour plots...")
-si_uptake_ctrl_bidir <- si_uptake_data %>% filter(bt_type == "Bidirectional") %>%
-  contour_plot_ctrl_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = bidir_palette_dense_ctrl, 
-                        breaks = seq(0,100,step_dense*100)) +
-  scale_x_continuous(name = "% of cases with\nchirping smartphones",
-                     labels = label_pc, breaks = seq(0,1,0.2)) +
-  scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
-                     breaks = seq(0,1,0.2)) +
-  facet_grid(row_lab~col_lab) +
-  theme_base + theme(legend.position="none")
-si_uptake_ctrl_fwd <- si_uptake_data %>% filter(bt_type == "Forward-only") %>%
-  contour_plot_ctrl_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = fwd_palette_dense_ctrl, 
-                        breaks = seq(0,100,step_dense*100)) +
-  scale_x_continuous(name = "% of cases with\nchirping smartphones",
-                     labels = label_pc, breaks = seq(0,1,0.2)) +
-  scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
-                     breaks = seq(0,1,0.2)) +
-  facet_grid(row_lab~col_lab) +
-  theme_base + theme(legend.position="none")
-si_uptake_ctrl <- plot_grid(si_uptake_ctrl_fwd %>% strip_upper(),
-                            si_uptake_ctrl_bidir %>% strip_lower(),
-                            nrow = 2, align = "v", axis = "l")
-
 cat("done.\n")
 
 #==============================================================================
@@ -350,36 +299,14 @@ cat("done.\n")
 
 cat("\nSaving output...")
 
-#plot_scale_cm <- snakemake@params[["panel_scale"]]
-plot_scale_cm <- 13
-plot_scale_in <- plot_scale_cm/2.54
-
 cat("\n\tMain figure...")
-#main_path <- snakemake@output[["main"]]
-main_path <- "output_files/dev_main_strategies.png"
 cowplot::save_plot(filename=main_path, plot=grid_main,
                    ncol = 3, nrow = 2, base_height = plot_scale_in,
                    base_asp = 0.8)
 
 cat("\n\tSI R_eff contour plots...")
-#si_uptake_reff_path <- snakemake@output[["si_uptake_reff"]]
-si_uptake_reff_path <- "output_files/dev_si_uptake_reff.png"
 cowplot::save_plot(filename=si_uptake_reff_path, plot=si_uptake_reff,
                    ncol = 3, nrow = 2, base_height = plot_scale_in,
                    base_asp = 0.83)
-
-cat("\n\tSI control-rate contour plots...")
-#si_uptake_ctrl_path <- snakemake@output[["si_uptake_ctrl"]]
-si_uptake_ctrl_path <- "output_files/dev_si_uptake_ctrl.png"
-cowplot::save_plot(filename=si_uptake_ctrl_path, plot=si_uptake_ctrl,
-                   ncol = 3, nrow = 2, base_height = plot_scale_in,
-                   base_asp = 0.83)
-
-cat("\n\tSI control-rate line plots...")
-#si_ptrace_ctrl_path <- snakemake@output[["si_ptrace_ctrl"]]
-si_ptrace_ctrl_path <- "output_files/dev_si_ptrace_ctrl.png"
-cowplot::save_plot(filename=si_ptrace_ctrl_path, plot=si_ctrl,
-                   ncol = 3, nrow = 1, base_height = plot_scale_in,
-                   base_asp = 0.65)
 
 cat("done.\n")
--- figures_workflow/scripts/main_strategies.R
+++ figures_workflow/scripts/main_strategies.R
@@ -17,6 +17,27 @@ theme_base <- theme_base + theme(
   axis.title.y = element_text(margin = margin(r = 0.15, unit = "cm")),
   axis.title.x = element_text(margin = margin(t = 0.2, b = 0, unit = "cm"))
 )
+
+# Input paths
+manual_path <- snakemake@input[["manual"]]
+digital_path <- snakemake@input[["digital"]]
+uptake_path <- snakemake@input[["uptake"]]
+window_path <- snakemake@input[["window"]]
+# manual_path <- "data/main_strategies_manual_1k_scenario.tsv.gz"
+# digital_path <- "data/main_strategies_digital_1k_scenario.tsv.gz"
+# uptake_path <- "data/main_strategies_uptake_1k_scenario.tsv.gz"
+# window_path <- "data/si_window_median_1k_scenario.tsv.gz"
+
+
+# Output paths & parameters
+plot_scale_cm <- snakemake@params[["panel_scale"]]
+main_path <- snakemake@output[["main"]]
+si_uptake_reff_path <- snakemake@output[["si_uptake_reff"]]
+# plot_scale_cm <- 13
+# main_path <- "output_files/dev_main_strategies.png"
+# si_uptake_reff_path <- "output_files/dev_si_uptake_reff.png"
+plot_scale_in <- plot_scale_cm/2.54
+
 cat("done.\n")
 
 #==============================================================================
@@ -26,8 +47,6 @@ cat("done.\n")
 cat("\nReading in data...")
 
 # Manual tracing (with and without automated)
-manual_path <- snakemake@input[["manual"]]
-#manual_path <- "data/main_strategies_manual_1k_scenario.tsv.gz"
 manual_data <- suppressMessages(read_tsv(manual_path)) %>%
   mutate(backtrace_distance = factor(backtrace_distance, levels=c(0,Inf)),
          contact_limit_manual = factor(contact_limit_manual, levels=c(6,2)),
@@ -36,8 +55,6 @@ manual_data <- suppressMessages(read_tsv(manual_path)) %>%
          p_traced = p_traced_manual)
 
 # Digital tracing alone (universal or high coverage)
-digital_path <- snakemake@input[["digital"]]
-#digital_path <- "data/main_strategies_digital_1k_scenario.tsv.gz"
 digital_data <- suppressMessages(read_tsv(digital_path)) %>%
   mutate(backtrace_distance = factor(backtrace_distance, levels=c(0,Inf)),
          contact_limit_auto = factor(contact_limit_auto, levels=c(Inf,2)),
@@ -45,12 +62,18 @@ digital_data <- suppressMessages(read_tsv(digital_path)) %>%
          p_traced = p_traced_auto)
 
 # Uptake contour plots
-uptake_path <- snakemake@input[["uptake"]]
-#uptake_path <- "data/main_strategies_uptake_1k_scenario.tsv.gz"
 uptake_data <- suppressMessages(read_tsv(uptake_path)) %>% 
   filter(p_traced_manual == 0.9 | contact_limit_manual == 6) %>%
   prep_contour_data(0.9, "p_smartphone_overall", "p_data_sharing_auto",
                     c("p_traced_manual", "backtrace_distance", "contact_limit_manual"))
+
+# Window contour plots
+
+window_data <- suppressMessages(read_tsv(window_path)) %>% 
+  filter(p_traced_auto != 0, p_smartphone_overall == 0.53,
+         backtrace_distance == Inf) %>%
+  index_data(key_x = "contact_limit_manual", key_y = "p_traced_manual") %>%
+  smooth_data(group_vars = c())
 
 cat("done.\n")
 
@@ -79,15 +102,31 @@ reff_manual <- manual_data %>%
 
 cat("\n\t2")
 
-reff_hybrid <- manual_data %>%
-  filter(p_traced_auto != 0, p_smartphone_overall == 0.8) %>%
+hybrid_data <- manual_data %>% filter(p_traced_auto != 0) %>%
+  mutate(bidir_type = ifelse(backtrace_distance == 0, "Forward tracing only",
+                             "Bidirectional tracing"),
+         uptake_pc = paste0(round(p_smartphone_overall * 100), "%"),
+         uptake_type = ifelse(backtrace_distance == 0 & p_smartphone_overall == 0.53,
+                              paste0(" (", uptake_pc, "\nsmartphone coverage)"),
+                              paste0(" (", uptake_pc, ")")),
+         label = paste0(bidir_type, uptake_type)) %>%
+  mutate(label = factor(label, levels = c("Forward tracing only (53%\nsmartphone coverage)",
+                                          "Forward tracing only (80%)",
+                                          "Bidirectional tracing (53%)",
+                                          "Bidirectional tracing (80%)")))
+reff_hybrid_raw <- hybrid_data %>%
   ggplot(aes(x=p_traced_manual, y=effective_r0_mean,
-             colour=backtrace_distance, 
+             colour=label, 
              linetype=contact_limit_manual,
              shape=contact_limit_manual)) %>%
-  format_reff(r0=2.5) %>% colour_backtrace %>% 
+  format_reff(r0=2.5) %>% #colour_backtrace %>% 
   linetype_window(label = label_limits_manual) %>% x_ptrace %>%
   theme_internal(pos = c(0.02, 0.01), yjust = 0, xjust = 0, vspace=vspace)
+reff_hybrid <- reff_hybrid_raw + 
+  scale_colour_manual(name = NULL, values = c("#1B9E77", "#7570B3", "#D95F02", "#E7298A")) +
+  guides(colour=guide_legend(ncol = 1, order = 1)) + theme(
+    legend.background = element_rect(fill = alpha("white", 0.8))
+  )
 
 cat("\n\t3")
 
@@ -99,39 +138,6 @@ reff_univ <- digital_data %>%
              shape=contact_limit_auto)) %>%
   format_reff(r0=2.5) %>% colour_backtrace %>% linetype_window %>% x_ptrace %>%
   theme_internal(pos = c(0.02, 0.01), yjust = 0, xjust = 0, vspace=vspace)
-
-cat("\n...done.\n")
-
-#------------------------------------------------------------------------------
-# Comparative control plot
-#------------------------------------------------------------------------------
-
-cat("\nGenerating comparative control plot...")
-
-cat("\n\tAssembling data...")
-digital_data_prep <- digital_data %>% filter(p_data_sharing_auto == 0.9,
-                                             contact_limit_auto == Inf) %>%
-  select(-contact_limit_auto)
-manual_data_prep <- manual_data %>% filter(contact_limit_manual == 6) %>%
-  select(-contact_limit_manual)
-comp_data <- bind_rows(manual_data_prep, digital_data_prep) %>%
-  filter(p_smartphone_overall == 0.8) %>%
-  mutate(backtrace_distance = factor(backtrace_distance, levels=c(Inf, 0)),
-         trace_type = factor(trace_type, 
-                             levels=c("Manual tracing only", "Manual + digital tracing",
-                                      "Digital tracing only")))
-
-cat("\n\tMaking plot...")
-ctrl_comp <- ggplot(comp_data,
-                    aes(x=p_traced, y=p_controlled,
-                        ymin=p_controlled_lower, ymax=p_controlled_upper,
-                        colour=trace_type,
-                        linetype=backtrace_distance,
-                        shape=backtrace_distance)) %>%
-  format_ctrl(ylimits = c(0,0.5), coord_ratio = 2) %>% 
-  colour_ttype(ncol=1, name=NULL) %>% 
-  linetype_backtrace(label=label_backtrace) %>% x_ptrace %>%
-  theme_internal(pos = c(0.02, 1), yjust = 1, xjust = 0, vspace=-1)
 
 cat("\n...done.\n")
 
@@ -172,13 +178,39 @@ reff_contour_hybrid <- uptake_data %>%
 
 # Add labels
 contour_label <- "Effective reprod. number, given\n90% prob. of trace success"
-label_contour <- function(contour_plot, size = 3.8, label=contour_label){
-  contour_plot + annotate("label", x=0.02, y=0.02, label=label,
-                          hjust = 0, vjust = 0, size = size,
+label_contour <- function(contour_plot, size = fontsize_base * 5/14,
+                          label=contour_label){
+  contour_plot + annotate("label", x=0.02, y=0.98, label=label,
+                          hjust = 0, vjust = 1, size = size,
                           label.size=NA, fill=alpha("white", 0.8))
 }
 reff_contour_digital_labelled <- label_contour(reff_contour_digital)
 reff_contour_hybrid_labelled <- label_contour(reff_contour_hybrid)
+
+cat("done.\n")
+
+#------------------------------------------------------------------------------
+# Window contour plot
+#------------------------------------------------------------------------------
+
+cat("\nGenerating trace-window contour plots...")
+
+window_contour <- window_data %>%
+  contour_plot_reff_abs("p_traced_manual", "contact_limit_manual",
+             palette = bidir_palette_sparse,
+             breaks =  seq(0,10,step_sparse)) +
+  scale_y_continuous(name = "Manual trace limit (days)", limits = c(0,10),
+                     breaks = seq(0,10,2)) +
+  scale_x_continuous(name = "Probability of trace\nsuccess (%)",
+                     limits = c(0,1), breaks = seq(0,1,0.2),
+                     labels = label_pc) +
+  theme(aspect.ratio = 1)
+
+label_window <- "Effective reprod. number, given\n53% smartphone coverage"
+window_contour_labelled <- window_contour +
+  annotate("label", x=0.02, y=9.8, label=label_window,
+           hjust = 0, vjust = 1, size = fontsize_base * 5/14,
+           label.size=NA, fill=alpha("white", 0.8))
 
 cat("done.\n")
 
@@ -189,13 +221,15 @@ cat("done.\n")
 cat("\nAssembling main figure...\n")
 
 grid_main <- plot_grid(reff_manual + ggtitle("Manual tracing only"),
-                       reff_univ + ggtitle("Digital tracing only\n(universal coverage)"),
+                       reff_univ + ggtitle("Digital tracing only\n(universal uptake)"),
                        reff_contour_digital_labelled +
-                         ggtitle("Bidirectional digital\ntracing (partial coverage)"),
+                         ggtitle("Effect of digital uptake\n(bidir. digital tracing)"),
                        reff_hybrid + ggtitle("Manual + digital\n(hybrid) tracing"),
-                       ctrl_comp + ggtitle("% outbreaks controlled"),
+                       # ctrl_comp + ggtitle("% outbreaks controlled"),
+                       window_contour_labelled + 
+                         ggtitle("Effect of tracing window\n (bidir. hybrid tracing)"),
                        reff_contour_hybrid_labelled + 
-                         ggtitle("Bidirectional hybrid\ntracing (partial coverage)"),
+                         ggtitle("Effect of digital uptake\n(bidir. hybrid tracing)"),
                        labels = "auto", nrow = 2, ncol = 3, align = "hv",
                        axis = "l", label_size = fontsize_base * fontscale_label,
                        label_fontfamily = titlefont, label_colour = "black")
@@ -207,45 +241,6 @@ cat("\n...done.\n")
 #==============================================================================
 
 #------------------------------------------------------------------------------
-# Single-strategy control-rate plots
-#------------------------------------------------------------------------------
-
-cat("\nGenerating single-strategy control-rate plots...")
-
-cat("\n\tPreparing data...")
-si_ctrl_data_digital <- digital_data %>%
-  filter(p_smartphone_overall == 1.0, p_data_sharing_auto == 1.0) %>%
-  mutate(trace_type = "Digital tracing only\n(universal coverage)",
-         contact_limit = as.character(contact_limit_auto)) %>%
-  select(-contact_limit_auto)
-
-si_ctrl_data_manual <- manual_data %>%
-  filter(p_smartphone_overall == 0.8) %>%
-  mutate(trace_type = ifelse(trace_type == "Manual + digital tracing",
-                             "Manual + digital\n(hybrid) tracing",
-                             trace_type),
-         contact_limit = as.character(contact_limit_manual)) %>%
-  select(-contact_limit_manual)
-
-si_ctrl_data <- bind_rows(si_ctrl_data_digital, si_ctrl_data_manual) %>%
-  mutate(contact_limit = factor(contact_limit, levels=c("Inf", "6", "2")))
-
-cat("\n\tMaking plot...")
-si_ctrl <- ggplot(si_ctrl_data,
-                    aes(x=p_traced, y=p_controlled,
-                        ymin=p_controlled_lower, ymax=p_controlled_upper,
-                        colour=backtrace_distance, 
-                        linetype=contact_limit,
-                        shape=contact_limit)) %>%
-  format_ctrl(ylimits = c(0,0.5), coord_ratio = 2) %>% colour_backtrace %>% 
-  linetype_window %>% x_ptrace %>%
-  theme_external()
-
-si_ctrl <- si_ctrl + facet_grid(.~trace_type)
-
-cat("\n...done.\n")
-
-#------------------------------------------------------------------------------
 # Generalised contour plots
 #------------------------------------------------------------------------------
 
@@ -254,7 +249,7 @@ cat("\nGenerating supplementary contour plots...")
 # Format data
 cat("\n\tPreparing data...")
 si_uptake_data <- uptake_data %>%
-  mutate(row_lab = paste(bt_type, "tracing,\n"),
+  mutate(row_lab = paste(bt_type, "tracing"),
          col_lab = ifelse(trace_type == "Digital only", "Digital tracing",
                           paste0("Hybrid tracing\n(", trace_window_manual, ")")))
 
@@ -276,7 +271,7 @@ fwd_palette_sparse <- uptake_data %>%
 cat("\n\tMaking R_eff contour plots...")
 si_uptake_reff_bidir <- si_uptake_data %>% filter(bt_type == "Bidirectional") %>%
   contour_plot_reff_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = bidir_palette_dense, breaks = seq(0,10,step_dense)) +
+                        palette = bidir_palette_sparse, breaks = seq(0,10,step_sparse)) +
   scale_x_continuous(name = "% of cases with\nchirping smartphones",
                      labels = label_pc, breaks = seq(0,1,0.2)) +
   scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
@@ -285,7 +280,7 @@ si_uptake_reff_bidir <- si_uptake_data %>% filter(bt_type == "Bidirectional") %>
   theme_base + theme(legend.position="none")
 si_uptake_reff_fwd <- si_uptake_data %>% filter(bt_type == "Forward-only") %>%
   contour_plot_reff_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = fwd_palette_dense, breaks = seq(0,10,step_dense)) +
+                        palette = fwd_palette_sparse, breaks = seq(0,10,step_sparse)) +
   scale_x_continuous(name = "% of cases with\nchirping smartphones",
                      labels = label_pc, breaks = seq(0,1,0.2)) +
   scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
@@ -296,32 +291,6 @@ si_uptake_reff <- plot_grid(si_uptake_reff_fwd %>% strip_upper(),
                             si_uptake_reff_bidir %>% strip_lower(),
                             nrow = 2, align = "v", axis= "l")
 
-# Ctrl SI plot
-cat("\n\tMaking control-rate contour plots...")
-si_uptake_ctrl_bidir <- si_uptake_data %>% filter(bt_type == "Bidirectional") %>%
-  contour_plot_ctrl_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = bidir_palette_dense_ctrl, 
-                        breaks = seq(0,100,step_dense*100)) +
-  scale_x_continuous(name = "% of cases with\nchirping smartphones",
-                     labels = label_pc, breaks = seq(0,1,0.2)) +
-  scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
-                     breaks = seq(0,1,0.2)) +
-  facet_grid(row_lab~col_lab) +
-  theme_base + theme(legend.position="none")
-si_uptake_ctrl_fwd <- si_uptake_data %>% filter(bt_type == "Forward-only") %>%
-  contour_plot_ctrl_abs("p_smartphone_overall", "p_data_sharing_auto",
-                        palette = fwd_palette_dense_ctrl, 
-                        breaks = seq(0,100,step_dense*100)) +
-  scale_x_continuous(name = "% of cases with\nchirping smartphones",
-                     labels = label_pc, breaks = seq(0,1,0.2)) +
-  scale_y_continuous(name = "% of cases sharing data", labels = label_pc,
-                     breaks = seq(0,1,0.2)) +
-  facet_grid(row_lab~col_lab) +
-  theme_base + theme(legend.position="none")
-si_uptake_ctrl <- plot_grid(si_uptake_ctrl_fwd %>% strip_upper(),
-                            si_uptake_ctrl_bidir %>% strip_lower(),
-                            nrow = 2, align = "v", axis = "l")
-
 cat("done.\n")
 
 #==============================================================================
@@ -330,36 +299,14 @@ cat("done.\n")
 
 cat("\nSaving output...")
 
-plot_scale_cm <- snakemake@params[["panel_scale"]]
-#plot_scale_cm <- 13
-plot_scale_in <- plot_scale_cm/2.54
-
 cat("\n\tMain figure...")
-main_path <- snakemake@output[["main"]]
-#main_path <- "output_files/dev_main_strategies.png"
 cowplot::save_plot(filename=main_path, plot=grid_main,
                    ncol = 3, nrow = 2, base_height = plot_scale_in,
                    base_asp = 0.8)
 
 cat("\n\tSI R_eff contour plots...")
-si_uptake_reff_path <- snakemake@output[["si_uptake_reff"]]
-#si_uptake_reff_path <- "output_files/dev_si_uptake_reff.png"
 cowplot::save_plot(filename=si_uptake_reff_path, plot=si_uptake_reff,
                    ncol = 3, nrow = 2, base_height = plot_scale_in,
                    base_asp = 0.83)
-
-cat("\n\tSI control-rate contour plots...")
-si_uptake_ctrl_path <- snakemake@output[["si_uptake_ctrl"]]
-#si_uptake_ctrl_path <- "output_files/dev_si_uptake_ctrl.png"
-cowplot::save_plot(filename=si_uptake_ctrl_path, plot=si_uptake_ctrl,
-                   ncol = 3, nrow = 2, base_height = plot_scale_in,
-                   base_asp = 0.83)
-
-cat("\n\tSI control-rate line plots...")
-si_ptrace_ctrl_path <- snakemake@output[["si_ptrace_ctrl"]]
-#si_ptrace_ctrl_path <- "output_files/dev_si_ptrace_ctrl.png"
-cowplot::save_plot(filename=si_ptrace_ctrl_path, plot=si_ctrl,
-                   ncol = 3, nrow = 1, base_height = plot_scale_in,
-                   base_asp = 0.65)
 
 cat("done.\n")
